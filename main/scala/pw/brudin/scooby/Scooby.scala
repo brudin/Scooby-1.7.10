df081cb2edc28d2d42ebd2f4094e2953657b9477
King Bowser <king@bowser.pw>
2015-03-21 01:50:10 -0500

    Removed redundant final modifier, added local  import for Mod.EventHandler

--- main/scala/pw/brudin/scooby/Scooby.scala
+++ main/scala/pw/brudin/scooby/Scooby.scala
@@ -8,12 +8,13 @@ import pw.brudin.scooby.mod.ModManager
 
 @Mod(modid = Scooby.id, modLanguage = "scala")
 object Scooby {
+  import Mod.EventHandler
 
-  final val id = "OpenComputers"
-  final val modManager = new ModManager(this)
-  final val eventManager = new EventManager(this)
+  val id = "OpenComputers"
+  val modManager = new ModManager(this)
+  val eventManager = new EventManager(this)
 
-  @Mod.EventHandler
+  @EventHandler
   def init(event: FMLInitializationEvent): Unit = {
     FMLCommonHandler.instance().bus().register(eventManager)
     MinecraftForge.EVENT_BUS.register(eventManager)
